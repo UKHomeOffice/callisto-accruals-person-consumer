0056aae5f47ddad90264e4c169d581cfd35e6e0b
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2023-03-15 11:45:12 +0000

    missed placeholder substitution

--- src/main/java/uk/gov/homeoffice/digital/sas/accrualspersonconsumer/CallistoAccrualsPersonConsumerApplication.java
+++ src/main/java/uk/gov/homeoffice/digital/sas/accrualspersonconsumer/CallistoAccrualsPersonConsumerApplication.java
@@ -1,4 +1,4 @@
-package uk.gov.homeoffice.digital.sas.callistoservice;
+package uk.gov.homeoffice.digital.sas.accrualspersonconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
